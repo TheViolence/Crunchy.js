1fb71fe3a70b6e37167d8227419a3c1851cd08bf
TheViolence <violenceplg7@gmail.com>
2020-03-08 14:13:18 +0100

    Reimplement with typescript

---
+++ crunchy.ts
@@ -0,0 +1,48 @@
+type ValueFactory = (cookieValue?: string, validityInDays?: number) => string;
+type ErrorFactory = () => string;
+
+interface Cookie {
+	key: string;
+	value: ValueFactory;
+	erase: ErrorFactory;
+}
+
+enum Action {
+	Get,
+	Set,
+}
+
+const Cookie = (key: string): Cookie => {
+	const value: ValueFactory = (cookieValue, validityInDays = 0) => {
+		const action: Action = cookieValue === undefined ? Action.Get : Action.Set;
+		switch (action) {
+			case Action.Get:
+				const cookies: string[] = document.cookie.split(`;`);
+				let output: string = ``;
+				cookies.forEach((cookie: string) => {
+					const cookiesLength: number = cookie.length;
+					while (cookie.charAt(0) == ` `)
+						cookie = cookie.substring(1, cookiesLength);
+					if (cookie.indexOf(key + `=`) === 0)
+						output = cookie.substring(key.length + 1, cookiesLength);
+				});
+				return output;
+			case Action.Set:
+				let expires: string = ``;
+				if (validityInDays) {
+					const exdate: Date = new Date();
+					exdate.setDate(exdate.getDate() + validityInDays);
+					expires = ` expires=${exdate.toUTCString()};`;
+				}
+				document.cookie = `${key}=${cookieValue};${expires}path=/`;
+				return cookieValue;
+		}
+	};
+	const erase: ErrorFactory = () =>
+		document.cookie = `${key}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/`;
+	return {
+		key,
+		value,
+		erase
+	};
+};
